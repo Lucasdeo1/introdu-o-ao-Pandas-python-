4d322412e5476051582c2cc827750a7714a45f11
Lucas Deodato Silva <113999313+Lucasdeo1@users.noreply.github.com>
2023-08-20 22:51:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import pandas as pd
+import numpy as np
 
-tabela = pd.read_csv('C:/Users/adm/OneDrive/Área de Trabalho/task/eleitorado/perfil_eleitorado_2020/perfil_eleitorado_2020.csv', sep=';', encoding='ISO-8859-1')
-tabela2 = pd.read_csv('C:/Users/adm/OneDrive/Área de Trabalho/task/resultados/resultadosSP_turno_1.csv', sep=';', encoding='ISO-8859-1')
+tabela = pd.read_csv('C:\\Users\\adm\OneDrive\\Área de Trabalho\\developments\\trarefaUsandoPandas\\introducao-ao-Pandas-python--main\\eleitorado\\perfil_eleitorado_2020\\perfil_eleitorado_2020.csv', sep=';', encoding='ISO-8859-1')
+tabela2 = pd.read_csv('C:\\Users\\adm\OneDrive\\Área de Trabalho\developments\\trarefaUsandoPandas\\introducao-ao-Pandas-python--main\\resultados\\resultadosSP_turno_1.csv', sep=';', encoding='ISO-8859-1')
 
 #print(tabela)
 #print(tabela2)
 
 
+tabelasJuntadas = pd.concat([tabela, tabela2], ignore_index=True)
 
-#tabelasJuntadas = pd.concat([tabela, tabela2], ignore_index=True , keys=['tabela', 'tabela2'])
-#tabelasJuntadas = pd.concat([tabela, tabela2], keys=['tabela', 'tabela2'])
-#print(tabelasJuntadas)
-tabelaMerge = pd.merge(tabela, tabela2)
+# Substituindo todos os valores -1 por NULO e NaN por '-' (sei lá estetica viu haha)
+tabelasJuntadas = tabelasJuntadas.replace(-1, 'NULO')
+tabelasJuntadas = tabelasJuntadas.replace(np.nan , '-')
 
-print(tabelaMerge)
+# Imprime tabela com eles modificados modificado
+print(tabelasJuntadas)
+
+candidato = input('selecione o canditado')
+
+candidato_df = tabelasJuntadas[tabelasJuntadas['NM_VOTAVEL'] == candidato]
+
+if not candidato_df.empty:
+    qt_eleitores = candidato_df.iloc[0]['QT_ELEITORES']
+    qt_eleitores = candidato_df.iloc[0]['QT_ELEITORES']
+    
+    print(f'O candidato {candidato} teve {qt_eleitores} eleitores.')
+else:
+    print(f'O candidato {candidato} não foi encontrado.')
+
+
+
